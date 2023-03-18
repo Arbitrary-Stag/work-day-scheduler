3645844b7ea80af77bb8a1010c4f951350d87395
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-03-17 20:31:59 -0700

    added js code for retrieving locally stored data and commented out js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Code is wrapped in a ready check to prevent the JS/jQuery from loading before all the html elements //
 $(document).ready(function() {
 
   var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-  var saveBtn = document.getElementsByClassName("fa-save");
+  var saveBtn = $(".btn")
 
+  // This code uses the array above as a means of comparing the id's to the current time, and then applying the class past, present, or future accordingly //
   hourArray.forEach(function (item) {
     let currentHour = dayjs().hour();
     if (item === currentHour) {
@@ -18,6 +16,7 @@ $(document).ready(function() {
     }
   });
 
+  // This code adds an event listener to all the save buttons in the document, then saves the id and text content of that div in local storage //
   saveBtn.on("click", function (event) {
     var saved = $(event.target);
 
@@ -27,7 +26,19 @@ $(document).ready(function() {
     localStorage.setItem(savedHour, JSON.stringify(savedEvent));
   });
 
+  // This code checks local storage for any saved items then loads the item in the correct text field based on the id //
+  hourArray.forEach(function (item) {
+    let hour = $(`#hour-${item}`);
+    let storedHourEvent = JSON.parse(localStorage.getItem(`hour-${item}`));
 
+    if (!storedHourEvent) {
+      hour.children().eq(1).val("");
+    } else {
+      hour.children().eq(1).val(`${storedHourEvent}`);
+    }
+  });
+
+  // This code uses dayjs to find the current date and then sets it as the text content of a specified html element //
   const today = dayjs().format('dddd, MMMM D YYYY');
   $('#currentDay').text("Today is: " + today);
 
