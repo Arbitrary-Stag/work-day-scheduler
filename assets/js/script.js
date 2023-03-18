6237fc5006cff3cd085eae6ff43c2e8cf98cd4a1
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-03-17 19:14:11 -0700

    add code for applying past, present, and future classes to hour blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,18 @@ $(document).ready(function() {
   var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   var saveBtn = document.getElementsByClassName("fa-save");
 
+  hourArray.forEach(function (item) {
+    let currentHour = dayjs().hour();
+    if (item === currentHour) {
+      $(`#hour-${item}`).attr("class", "row time-block present");
+    } else if (item > currentHour) {
+      $(`#hour-${item}`).attr("class", "row time-block future");
+    } else {
+      $(`#hour-${item}`).attr("class", "row time-block past");
+    }
+  });
+
   const today = dayjs().format('dddd, MMMM D YYYY');
   $('#currentDay').text("Today is: " + today);
-
-  const currentHour = dayjs().hour();
 
 });
